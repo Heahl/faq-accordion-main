c84396832fcc213678602bda09165b155c78008e
heahl <hendrik712.a@gmail.com>
2024-05-18 19:31:03 +0200

    final commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={GeistSans.className}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,30 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      backgroundImage: {
+        'mobile-bg': "url('/assets/images/background-pattern-mobile.svg')",
+        'desktop-bg': "url('/assets/images/background-pattern-desktop.svg')",
       },
+      backgroundSize: {
+        'full-auto' : '100% auto',
+      },
+      colors: {
+        'white' : 'hsl(0, 0%, 100%)',
+        'light-pink' : 'hsl(275, 100%, 97%)',
+        'grayish-purple' : 'hsl(292, 16%, 49%)',
+        'dark-purple' : 'hsl(292, 42%, 14%)',
+      },
+      fontFamily: {
+        'work-sans' : ['Work Sans', 'sans-serif'],
+      },
+      fontWeight: {
+        'normal' : '400',
+        'semibold' : '600',
+        'bold' : '700',
+      }
     },
   },
   plugins: [],
